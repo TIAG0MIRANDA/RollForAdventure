1366d0e8339174d1230193337096bad79c004343
Tiago Miranda <t.miranda0249@gmail.com>
2024-10-16 13:06:12 -0300

    Tudo pronto

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ document.querySelectorAll('.dice-buttons button').forEach(button => {
     button.addEventListener('click', () => {
         const sides = parseInt(button.id.slice(1)); // Extrai o número do ID do botão
         const result = Math.floor(Math.random() * sides) + 1; // Gera um número aleatório
-        document.getElementById('output').innerText = result; // Atualiza o resultado na tela
+        const outputElement = document.getElementById('output');
+
+        // Atualiza o resultado na tela
+        outputElement.innerText = result; 
+
+        // Verifica se o resultado é o máximo
+        if (result === sides) {
+            outputElement.classList.add('max-result');
+        } else {
+            outputElement.classList.remove('max-result'); // Remove a classe se não for crítico
+        }
     });
 });
